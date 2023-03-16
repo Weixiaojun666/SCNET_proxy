d6e9a614213bd44736f9bc8e3e1b775f20a061ad
Weixiaojun <weixiaojun666@vip.qq.com>
2023-03-16 19:49:59 +0800

    修复一堆bug

--- src/main/java/com/weiservers/Thread/Child/Cache.java
+++ src/main/java/com/weiservers/Thread/Child/Cache.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 
 import java.net.DatagramPacket;
 
-import static com.weiservers.Cloud.Cloud.postserverinfo;
+import static com.weiservers.Cloud.Cloud.postmotd;
 import static com.weiservers.Core.Tools.getServerInfo;
 
 public class Cache extends Thread {
@@ -32,7 +32,7 @@ public class Cache extends Thread {
                 motd.setMotd(bytes);
                 motd.setTime(System.currentTimeMillis());
                 getServerInfo(motd);
-                postserverinfo(motd);
+                postmotd(motd);
             }
         } catch (Exception e) {
             if (!isInterrupted()) {
